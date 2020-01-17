4aae0bb9550f1a4872851b0bfa4c18b0d25ac497
Michael Thompson <Michael.Thompson@sportsbet.com.au>
2020-01-17 12:50:15 +1100

    Step2 - Added calculator Add feature

---
+++ Calculator.Tests.Features/CalculatorSteps.cs
@@ -0,0 +1,38 @@
+﻿using CalculatorDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Calculator.Tests.Features
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private RpnCalculator _calculator = new RpnCalculator();
+        private double _result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(double p0)
+        {
+            _calculator.EnterNumber(p0);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _result = _calculator.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(double p0)
+        {
+            Assert.AreEqual(_result, p0);
+        }
+
+        [Then(@"current value is (.*)")]
+        public void ThenCurrentValueIs(double p0)
+        {
+            Assert.AreEqual(_calculator.CurrentValue(), p0);
+        }
+    }
+}
---
+++ CalculatorDemo/RpnCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorDemo
+{
+    public class RpnCalculator
+    {
+        private Stack<double> _stack = new Stack<double>();
+
+        public void EnterNumber(double n)
+        {
+            _stack.Push(n);
+        }
+
+        public double Add()
+        {
+            var result = _stack.Pop() + _stack.Pop();
+            _stack.Push(result);
+            return result;
+        }
+
+        public double CurrentValue()
+        {
+            return _stack.Peek();
+        }
+    }
+}
